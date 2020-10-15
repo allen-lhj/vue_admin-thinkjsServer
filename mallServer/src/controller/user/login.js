57fdab8721fabf43a98702362cd2f4f135d6d05c
allen-lhj <liu772636821@163.com>
2020-10-15 18:42:29 +0800

    a

--- mallServer/src/controller/user/login.js
+++ mallServer/src/controller/user/login.js
@@ -8,10 +8,10 @@ module.exports = class extends Base {
   async loginAction() {
     if (this.method === 'POST') {
       //   获取post提交用户名和密码
-      let username = this.post('username');
-      let password = this.post('password');
+      const username = this.post('username');
+      const password = this.post('password');
       // 查寻数据库member表,根据用户名查询用户信息
-      let user = await this.model('member').where({username: username}).find();
+      const user = await this.model('member').where({username: username}).find();
       // 判断提交的密码是否与查询到的密码一致
       if (user.password === this.verifyPassword(password)) {
         // 完成登陆-更新token
--- vue-admin/src/utils/validate.js
+++ vue-admin/src/utils/validate.js
@@ -14,6 +14,7 @@ export function isExternal(path) {
  * @param {string} str
  * @returns {Boolean}
  */
+// 账号的规则
 export function validUsername(str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
